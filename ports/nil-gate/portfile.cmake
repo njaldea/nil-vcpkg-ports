837af2f2b03895aa3c32e9ec44aa5daf2564f807
Neil Aldea <njaldea@gmail.com>
2024-04-14 15:52:34 -0400

    [ports] nil/nil-gate

--- ports/nil-gate/portfile.cmake
+++ ports/nil-gate/portfile.cmake
@@ -1,19 +1,14 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO njaldea/nil-gate
-    REF c6bf7c6ce42765686156d437c849c28ec37df28a
-    SHA512 3316647217dcbdbdd94da0c2346df49383b7c5a2791d7b5b1980b0920e265a457215fb3fec4bb70507a37b2e48937606f8c3f026546d69596e3a2a531db51b6c
+    REF bb3cfc49a79ba7e1824f2ed153a8dcc8d7e6a680
+    SHA512 4ca60cbca5b567b1c8c7cdf234e95b460f4299655bf13ace9546b9e9346ce5f741b1993ffb5e4f15ee574955fe90e5ccb073cd56c030429d79c605c6467847a4
     HEAD_REF master
 )
 
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup()
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
-
-file(
-    INSTALL "${SOURCE_PATH}/LICENSE"
-    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
-    RENAME copyright
-)
