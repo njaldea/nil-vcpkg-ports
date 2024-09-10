4a388720f58ef34e3335be7f81c825d25b3cf215
Neil Aldea <njaldea@gmail.com>
2024-09-10 09:59:54 -0400

    [xit] fix cmake.in

--- ports/nil-xit/portfile.cmake
+++ ports/nil-xit/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO njaldea/nil-xit
-    REF de209d65efd36e7cc524adc47c3a4dd5fa3a553c
-    SHA512 cf152d04a71108e9292f32a5b1c0dfe09ac150042cf5467be54bf9c1347aa82c3d5a8fcbf8b50d41ba475a704b8ae027bda1c091d45fbf17f69301b672c69255
+    REF bb727b440156e8e37faf7aad21192b8bf1c3a860
+    SHA512 f0edccabeebb31e77dcf7c495b86a7092b5a311cb232509d744790cfb3d7dbb4670979a5d7b16e7e6f371f8cb5acf2932de2efa2c6ea44c21077b9e65e36aa18
     HEAD_REF master
 )
 
