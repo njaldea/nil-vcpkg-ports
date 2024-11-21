e1d6ae5f68cb07228a771874401f5184dd6e25af
Neil Aldea <njaldea@gmail.com>
2024-11-21 13:44:59 +0800

    [xit-test] fix portfile (passed enabled features)

--- ports/nil-xit-test/portfile.cmake
+++ ports/nil-xit-test/portfile.cmake
@@ -11,7 +11,10 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
         gtest          ENABLE_FEATURE_GTEST
 )
 
-vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS ${FEATURE_OPTIONS}
+)
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup()
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
