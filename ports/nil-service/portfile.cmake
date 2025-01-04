cbd0f525c1a4e74e7d88e6009af072f08e697a44
Neil Aldea <njaldea@gmail.com>
2025-01-04 21:38:22 +0800

    [service] updated porfile

--- ports/nil-service/portfile.cmake
+++ ports/nil-service/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO njaldea/nil-service
-    REF 891d5bb434ecfe20bbfff530a80fa0f45ee950d2
-    SHA512 4ad44efb1cbcc61addd34cae3ef7b6138fdfcf51a40edb8f33e7ccea3de3116e9825c172b0c85c3d36f08c0265f6756cd15c3d2821b73c8cd05e34b59ee8e4a1
+    REF 0ec3d243e961166775695358f7da7656dbd917be
+    SHA512 3f0ea96e27a94f065c4f8360e7bac721958c1b2a7d24b8cb8587d923185bb01ca510b1b9e603d25acbc9195a7b8d14fe9849b49cc7b0b8a1e3e7d640b8c6c8d8
     HEAD_REF master
 )
 
