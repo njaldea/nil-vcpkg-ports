63a82a44ed295510d77bdcf8ccb3547b7b98035d
Neil Aldea <njaldea@gmail.com>
2025-06-15 20:54:17 +0800

    [gate] added traits::compare [gate] v1.1.0

--- ports/nil-gate/portfile.cmake
+++ ports/nil-gate/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO njaldea/nil-gate
-    REF 774a75ec67a4d4c5988a84716f016f6d3fffce8b
-    SHA512 8f1140c689e8c1d0fbb3bb44a3e5d6a592ed4dc0e489789eee2a6c44858b30d3ceb679905a6aacbc60f1a6bef5b262cecbbede04c97507e469c4117119105589
+    REF 62869b0247b79f4bbd4de9e3deb30c98f86b0f9c
+    SHA512 871f14b9e1ee6e512172da2873684a0aaf73ddceed0b12154b5c1a2ea8f7aec9b79bbe5360dad20c30a456bbe79c6c15789978d1aaa71ccbdf57b91fab22f577
     HEAD_REF master
 )
 
