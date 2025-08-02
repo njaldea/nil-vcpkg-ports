8a81a9c5ef282faea30f251e0e278ba09c3baee3
Neil Aldea <njaldea@gmail.com>
2025-08-02 10:13:10 +0800

    [xalt] updated portfile (v1.2.0)

--- ports/nil-xalt/portfile.cmake
+++ ports/nil-xalt/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO njaldea/nil-xalt
-    REF v1.1.0
-    SHA512 f65857e56ba932d65f7c52180d14442df46b66a5fc7c31bb380f58a828290b6cfbf079b66f54e90137a98a9e8a7e32798ceccb81fb756a45567ebcc2159c93b7
+    REF v1.2.0
+    SHA512 cccdbb554eb355b173079a0badecda30526ea2a1c2b2925da67a04c53697f950fc70f04f965ed61f27a051f9cd1a1f521eadbae473f79b5640d6070ada05f5f2
     HEAD_REF master
 )
 
