24580914f83f9f651d23a2fcc5e3dbc083a65863
Neil Aldea <njaldea@gmail.com>
2026-03-01 16:34:29 +0800

    moved cpack.cmake in

--- ports/nil-cmake/portfile.cmake
+++ ports/nil-cmake/portfile.cmake
@@ -7,4 +7,13 @@ vcpkg_from_github(
 )
 
 include(${CMAKE_CURRENT_LIST_DIR}/port.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/../cpack.cmake)
+
+if (DEFINED ENV{NIL_BUILD_CPACK_DEB} AND "$ENV{NIL_BUILD_CPACK_DEB}" STREQUAL "1")
+    set(BUILD_DIR "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel")
+    message(STATUS "Running CPack in build directory ${BUILD_DIR}")
+
+    execute_process(
+        COMMAND cpack -G DEB -D CPACK_OUTPUT_FILE_PREFIX=$ENV{NIL_CPACK_OUT_DIR}
+        WORKING_DIRECTORY ${BUILD_DIR}
+    )
+endif()
