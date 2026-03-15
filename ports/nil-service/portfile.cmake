7663bcf50c90b3671e286ed83a9723f723b4a0f0
Neil Aldea <njaldea@gmail.com>
2026-03-15 14:17:25 +0800

    [nil-gate]: v1.6.4 [nil-service]: v1.0.10 [nil-xit]: v0.3.7 updated git-clone script

--- ports/nil-service/portfile.cmake
+++ ports/nil-service/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO "njaldea/${PORT}"
     REF "v${VERSION}"
-    SHA512 ed46e4b929eb042831bf97e3cdfd4d7bbd6cf4d2da3bd1d5f1bab42d23d7f51292c15b74f5b4810dd8ad1374e3f512d4ccb9c5808f48e339637f842de8106b14
+    SHA512 47de2fcd265270944e8ac4bab62f8844d6e1d5fb1d8ef351befdc8ee725f5791809ffe437a53272b52cc419c8a307164218cc84b4e1cfd8ae3bdc23f907cfce8
     HEAD_REF master
 )
 
