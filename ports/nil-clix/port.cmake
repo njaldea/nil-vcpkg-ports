95dd929a454e4d560df63d2b55d143bb0aa23b14
Neil Aldea <njaldea@gmail.com>
2026-04-03 13:03:32 +0800

    [nil-clix]: v1.0.7

--- ports/nil-clix/port.cmake
+++ ports/nil-clix/port.cmake
@@ -1,7 +1,23 @@
-vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        c-api          ENABLE_C_API
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS ${FEATURE_OPTIONS}
+)
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${CURRENT_PACKAGES_DIR}/share/doc/${PORT}/LICENSE")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+set(USAGE_FILE ${CURRENT_PACKAGES_DIR}/share/${PORT}/usage)
+file(WRITE  ${USAGE_FILE} "nil-clix provides CMake targets:\n\n")
+file(APPEND ${USAGE_FILE} "    find_package(nil-clix CONFIG REQUIRED)\n")
+file(APPEND ${USAGE_FILE} "    target_link_libraries(TARGET PUBLIC nil::clix)\n\n")
+file(APPEND ${USAGE_FILE} "    # for nil::clix-c-api\n")
+file(APPEND ${USAGE_FILE} "    find_package(nil-clix CONFIG REQUIRED COMPONENTS c-api)\n")
+file(APPEND ${USAGE_FILE} "    target_link_libraries(TARGET PUBLIC nil::clix-c-api)\n\n")
